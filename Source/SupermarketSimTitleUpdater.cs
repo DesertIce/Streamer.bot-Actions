b909b73db5fb00c4518bbce0f7463659dd67010e
DesertIce <DesertIceTTV@gmail.com>
2024-03-30 17:26:29 -0400

    Added support multi PC setups. Should be able to use network drive and simple folder path setting.

--- Source/SupermarketSimTitleUpdater.cs
+++ Source/SupermarketSimTitleUpdater.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 public class CPHInline
 {
@@ -9,17 +11,29 @@ public class CPHInline
 			var lastPipeIndex = currentTitle.LastIndexOf("|");
 			if(lastPipeIndex > 0)
 			{
-				if(CPH.TryGetArg<string>("fullPath", out var saveFilePath))
+				var saveFilePath = string.Empty;
+				if(CPH.TryGetArg<string>("fullPath", out saveFilePath)){}
+				else if(CPH.TryGetArg<string>("saveFolderPath", out var saveFolderPath))
 				{
-					currentTitle = currentTitle.Remove(lastPipeIndex, currentTitle.Length - lastPipeIndex);
-					var saveFileJSON = JObject.Parse(File.ReadAllText(saveFilePath));
-					var progressionData = saveFileJSON["Progression"]["value"];
-					var money = progressionData.Value<decimal>("Money");
-					var level = progressionData.Value<int>("CurrentStoreLevel");
-					var day = progressionData.Value<int>("CurrentDay");
-					currentTitle = $"{currentTitle}| Day: {day} Level: {level} Money: {money:C}";
-					CPH.SetChannelTitle(currentTitle);
+					var directory = new DirectoryInfo(saveFolderPath);
+					var fileInfo = directory.GetFiles("*.es3").OrderByDescending(f => f?.LastWriteTime ?? DateTime.MinValue).FirstOrDefault();
+					saveFilePath = fileInfo?.FullName;
 				}
+				
+				if(string.IsNullOrWhiteSpace(saveFilePath))
+				{
+					CPH.LogError("No valid file path could be determined for Super Market Sim");
+					return false;
+				}
+				
+				currentTitle = currentTitle.Remove(lastPipeIndex, currentTitle.Length - lastPipeIndex);
+				var saveFileJSON = JObject.Parse(File.ReadAllText(saveFilePath));
+				var progressionData = saveFileJSON["Progression"]["value"];
+				var money = progressionData.Value<decimal>("Money");
+				var level = progressionData.Value<int>("CurrentStoreLevel");
+				var day = progressionData.Value<int>("CurrentDay");
+				currentTitle = $"{currentTitle}| Day: {day} Level: {level} Money: {money:C}";
+				CPH.SetChannelTitle(currentTitle);
 			}
 		}
 		return true;
